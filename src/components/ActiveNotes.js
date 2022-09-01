da81c97bf82c5acb3c65f72a9935a9aaa5d1cfb2
Siti Iedrania <13519137@std.stei.itb.ac.id>
2022-09-01 18:50:44 +0700

    refactor: minor fixes

--- src/components/ActiveNotes.js
+++ src/components/ActiveNotes.js
@@ -3,13 +3,13 @@ import NoteItem from './NoteItem';
 
 function ActiveNotes({ notes, onDelete }) {
   if (Object.keys(notes).length === 0) {
-    return <p class="notes-list__empty-message">Tidak ada catatan</p>
+    return <p className="notes-list__empty-message">Tidak ada catatan</p>
   } else {
     return (
       <div className='notes-list'>
         {
           notes.map((note) => (
-            <NoteItem {...note} key={note.createdAt} id={note.id} onDelete={onDelete} />
+            <NoteItem {...note} key={note.id} id={note.id} onDelete={onDelete} />
           ))
         }
       </div>
--- src/components/ArchivedNotes.js
+++ src/components/ArchivedNotes.js
@@ -2,7 +2,7 @@ import React from 'react';
  
 function ArchivedNotes() {
   return (
-    <p class="notes-list__empty-message">Tidak ada catatan</p>
+    <p className="notes-list__empty-message">Tidak ada catatan</p>
   );
 }
  

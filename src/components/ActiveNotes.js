ec780f1108a2fbf60fe0700ea9f3c16f51cbbef1
Siti Iedrania <13519137@std.stei.itb.ac.id>
2022-09-01 17:45:40 +0700

    feat: add empty conditional

--- src/components/ActiveNotes.js
+++ src/components/ActiveNotes.js
@@ -2,15 +2,19 @@ import React from 'react';
 import NoteItem from './NoteItem';
 
 function ActiveNotes({ notes, onDelete }) {
-  return (
-    <div className='notes-list'>
-      {
-        notes.map((note) => (
-          <NoteItem {...note} key={note.createdAt} id={note.id} onDelete={onDelete} />
-        ))
-      }
-    </div>
-  );
+  if (Object.keys(notes).length === 0) {
+    return <p class="notes-list__empty-message">Tidak ada catatan</p>
+  } else {
+    return (
+      <div className='notes-list'>
+        {
+          notes.map((note) => (
+            <NoteItem {...note} key={note.createdAt} id={note.id} onDelete={onDelete} />
+          ))
+        }
+      </div>
+    );
+  }
 }
 
 export default ActiveNotes;
